0651d3a0a29e888e282dc6e8bfd1d4ef117d24be
Cristiane Freire <cristiane.p.freire@hotmail.com>
2016-01-12 02:57:24 -0400

    ProfileBean update to use rowEdit

--- scrumtaskboard/src/br/com/scrumtaskboard/control/mb/ProfileBean.java
+++ scrumtaskboard/src/br/com/scrumtaskboard/control/mb/ProfileBean.java
@@ -3,8 +3,12 @@ package br.com.scrumtaskboard.control.mb;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.event.RowEditEvent;
 
 import br.com.scrumtaskboard.model.bean.Profile;
 import br.com.scrumtaskboard.model.dao.DAO;
@@ -45,10 +49,28 @@ public class ProfileBean {
 	public void delete(){
 		DAO<Profile> dao = new DAO<Profile>(Profile.class);
 		dao.delete(this.profile);
+		
 		this.profile = new Profile();
+		this.profiles = dao.listAll();
 	}
 	
 	public void setProfile(Profile profile){
 		this.profile = profile;
 	}
+	
+	public void onRowEdit(RowEditEvent event) {
+        FacesMessage msg = new FacesMessage("Profile Edited", ((Profile) event.getObject()).getId().toString());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+        
+        DAO<Profile> dao = new DAO<Profile>(Profile.class);
+		dao.update(((Profile) event.getObject()));
+		
+		this.profile = new Profile();
+		this.profiles = dao.listAll();
+    }
+     
+    public void onRowCancel(RowEditEvent event) {
+        FacesMessage msg = new FacesMessage("Edit Cancelled", ((Profile) event.getObject()).getId().toString());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
 }
